58c10999e8f42ad299a62cf33f4d38a9d98f2d54
MaximilianRieder <maximilian.rieder@t-online.de>
2025-07-10 16:44:20 +0200

    inital

---
+++ src/main/java/systema/demo/amq/SimpleListenerCount.java
@@ -0,0 +1,198 @@
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnection;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.jms.client.ActiveMQMessageConsumer;
+import org.apache.activemq.artemis.jms.client.ActiveMQSession;
+
+/**
+ * Simple listener application, that counts the received and missed messages.
+ *
+ * @author Maximilian Rieder
+ */
+public class SimpleListenerCount implements MessageListener
+{
+	ActiveMQConnectionFactory connectionFactory = null;
+	ActiveMQConnection connection = null;
+	ActiveMQMessageConsumer consumer = null;
+	ActiveMQSession session = null;
+	String brokerUrl = "(tcp://DEREGDEVBUS01:6666,tcp://DEREGDEVBUS02:6666)?failoverAttempts=-1";
+	String topicName = "count.topic";
+	boolean exited = false;
+	int lastCount = 0;
+	int missedMessages = 0;
+
+	public SimpleListenerCount()
+	{
+	}
+
+	/**
+	 * checks if the message that was previously received is the current integer - 1
+	 *
+	 * @param message received message (TextMessage as single integer)
+	 */
+	public void onMessage(Message message)
+	{
+		try
+		{
+			String messageString = ((TextMessage) message).getText();
+			if ( "exit".equals(messageString) )
+			{
+				shutdown();
+				return;
+			}
+			int messageCount = 0;
+			try
+			{
+				messageCount = Integer.parseInt(messageString);
+			}
+			catch ( NumberFormatException e )
+			{
+				System.err.println("message could not be parsed to int.");
+				return;
+			}
+			if ( lastCount == 0 )
+			{
+				lastCount = messageCount;
+				System.out.println("handleEventNow started counter at: " + lastCount);
+				return;
+			}
+			if ( (lastCount + 1) == messageCount )
+			{
+				System.out.println("*********  handleEventNow received message with count: " + messageCount +
+				                   " correctly. Total missed messages = " + missedMessages);
+			}
+			else
+			{
+				missedMessages = missedMessages + (messageCount - lastCount);
+				System.out.println("********* previous count=" + lastCount +
+				                   " current count=" + messageCount +
+				                   " missed total messages since startup: " + missedMessages);
+			}
+			lastCount = messageCount;
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void subscribe(String subject) throws JMSException
+	{
+		Destination topic = session.createTopic(subject);
+		consumer = (ActiveMQMessageConsumer) session.createConsumer(topic);
+		consumer.setMessageListener(this);
+		System.out.println("listening to:" + topic);
+	}
+
+	/**
+	 * Connect to a broker and start the connection
+	 *
+	 * @throws JMSException if connection fails
+	 */
+	public void connect() throws JMSException
+	{
+		System.out.println("Attempt to create authentication disabled connection factory.");
+		connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		System.out.println("Attempt to create  connection");
+		connection = (ActiveMQConnection) connectionFactory.createConnection();
+		System.out.println("Connection established.");
+		connection.start();
+		System.out.println("Connection started.");
+		session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		System.out.println("Session created.");
+	}
+
+	/**
+	 * stop and close
+	 */
+	private void shutdown()
+	{
+		try
+		{
+			if ( consumer != null )
+			{
+				consumer.close();
+				System.out.println("Consumer closed.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			if ( session != null )
+			{
+				session.close();
+				System.out.println("Session closed.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			if ( connection != null )
+			{
+				connection.stop();
+				connection.close();
+				System.out.println("Connection stopped.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+		if ( connection != null )
+		{
+			connectionFactory.close();
+			System.out.println("ConnectionFactory closed.");
+		}
+		exited = true;
+	}
+
+	/**
+	 * Start the count listener (parameters are optional)
+	 *
+	 * @param args 0: broker url
+	 *             1: topic
+	 * @throws JMSException
+	 */
+	public static void main(String[] args) throws JMSException
+	{
+		SimpleListenerCount client = new SimpleListenerCount();
+
+		client.connect();
+		if ( args.length == 1 )
+		{
+			client.brokerUrl = args[0];
+		}
+		if ( args.length == 2 )
+		{
+			client.topicName = args[1];
+		}
+		try
+		{
+			client.connect();
+			client.subscribe(client.topicName);
+			while ( !client.exited )
+			{
+				Thread.sleep(100);
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			client.shutdown();
+		}
+	}
+}
---
+++ src/main/java/systema/demo/amq/SimplePublisherCount.java
@@ -0,0 +1,169 @@
+import javax.jms.DeliveryMode;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnection;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.jms.client.ActiveMQMessageProducer;
+import org.apache.activemq.artemis.jms.client.ActiveMQSession;
+
+/**
+ * Simple publisher application, that publishes an incrementing counter as TextMessage continuously.
+ *
+ * @author Maximilian Rieder
+ */
+public class SimplePublisherCount
+{
+	private ActiveMQConnectionFactory connectionFactory = null;
+	private ActiveMQConnection connection = null;
+	private String brokerUrl = "(tcp://DEREGDEVBUS01:6666,tcp://DEREGDEVBUS02:6666)?failoverAttempts=-1";
+	private String topicName = "count.topic";
+	private long ttlOnTopics = 180000;
+	private ActiveMQSession session;
+	private ActiveMQMessageProducer producer;
+	private int interval = 2000;
+	private int count = 0;
+
+	public SimplePublisherCount()
+	{
+	}
+
+	/**
+	 * Connect to a broker and start the connection
+	 *
+	 * @throws JMSException if connection fails
+	 */
+	public void connect() throws JMSException
+	{
+		System.out.println("Attempt to create authentication disabled connection factory.");
+		connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		System.out.println("Attempt to create  connection");
+		connection = (ActiveMQConnection) connectionFactory.createConnection();
+		System.out.println("Connection established.");
+		connection.start();
+		System.out.println("Connection started.");
+
+		session = (ActiveMQSession) connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		producer = (ActiveMQMessageProducer) session.createProducer(null);
+		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		producer.setPriority(Message.DEFAULT_PRIORITY);
+		producer.setTimeToLive(ttlOnTopics);
+		System.out.println("Producer instantiated.");
+	}
+
+	/**
+	 * stop and close
+	 */
+	public void shutdown()
+	{
+		try
+		{
+			if ( producer != null )
+			{
+				producer.close();
+				System.out.println("Producer closed.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			if ( session != null )
+			{
+				session.close();
+				System.out.println("Session closed.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+
+		try
+		{
+			if ( connection != null )
+			{
+				connection.stop();
+				System.out.println("Connection stopped.");
+				connection.close();
+				System.out.println("Connection closed.");
+			}
+		}
+		catch ( JMSException e )
+		{
+			e.printStackTrace();
+		}
+
+		if ( connection != null )
+		{
+			connectionFactory.close();
+			System.out.println("ConnectionFactory closed.");
+		}
+	}
+
+	/**
+	 * Starts endlessly publishing an incrementing counter.
+	 *
+	 * @param subject defines the topic that is published to.
+	 * @throws JMSException if something fails.
+	 */
+	public void startPublishLoop(String subject) throws JMSException
+	{
+		Topic topic = session.createTopic(subject);
+
+		while ( true )
+		{
+			TextMessage jmsMessage = session.createTextMessage();
+			jmsMessage.setText(String.valueOf(count));
+			producer.send(topic, jmsMessage);
+			System.out.println("Message published. " + jmsMessage);
+			count++;
+			try
+			{
+				Thread.sleep(this.interval);
+			}
+			catch ( InterruptedException e )
+			{
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Start the count publisher (parameters are optional)
+	 *
+	 * @param args 0: broker url
+	 *             1: topic
+	 */
+	public static void main(String[] args)
+	{
+		SimplePublisherCount client = new SimplePublisherCount();
+		if ( args.length == 1 )
+		{
+			client.brokerUrl = args[0];
+		}
+		if ( args.length == 2 )
+		{
+			client.topicName = args[1];
+		}
+		try
+		{
+			client.connect();
+			client.startPublishLoop(client.topicName);
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			client.shutdown();
+		}
+	}
+}
